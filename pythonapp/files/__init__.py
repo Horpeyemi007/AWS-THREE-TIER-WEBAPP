1080340415e1edfc1e92739b8a101cdd0bc40c64
Opeseen <Opeyemi5055@yahoo.com>
2024-01-15 23:43:51 +0100

    Added source code

---
+++ pythonapp/files/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy;
+import mysql.connector
+from .config import connection
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'admin'
+    app.config['SQLALCHEMY_DATABASE_URI'] = connection
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
